f3f608087ffd7d6053080a9a0d9313c6e0839944
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-09-26 21:56:29 +0300

    commit tree 3a7d408b2e41e42787b3902fb6c3f6d71c9d2170

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,50 @@
+class SpicesController < ApplicationController
+    #get
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def show
+        spice = Spice.find_by(id: params[:id])
+        if spice
+         render json: spice
+        else
+          render json: {error: "spice not found"}, status: :not_found
+        end
+    end
+  #delete
+    def destroy
+        spice = Spice.find_by(id: params[:id])
+        if spice
+          spice.destroy
+          head :no_content
+        else
+            render json: {error: "spice not found"}, status: :not_found
+        end
+    end
+
+    #create
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    #update
+    def update
+        spice = Spice.find_by(id: params[:id])
+        if spice 
+            spice.update(spice_params)
+            render json: spice
+        else
+            render json: {error: "spice not found"}, status: :not_found
+        end
+    end
+
+    private
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    
+end
